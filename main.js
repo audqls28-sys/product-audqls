56fce568c4ca5f3906cf3165b46a96a1b3341a1d
audqls28 <audqls28@gmail.com>
2026-04-19 05:33:36 +0000

    Translate to Korean and add Dark/Light mode

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const lottoContainer = document.getElementById('lotto-container');
 const generateBtn = document.getElementById('generate-btn');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const themeText = document.getElementById('theme-text');
 
+// 로또 번호 생성 함수
 const generateNumbers = () => {
     const numbers = new Set();
     while (numbers.size < 6) {
@@ -9,6 +12,7 @@ const generateNumbers = () => {
     return Array.from(numbers).sort((a, b) => a - b);
 };
 
+// 번호 표시 함수
 const displayNumbers = (numbers) => {
     lottoContainer.innerHTML = '';
     for (const number of numbers) {
@@ -19,9 +23,37 @@ const displayNumbers = (numbers) => {
     }
 };
 
+// 테마 변경 함수
+const switchTheme = (e) => {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        themeText.textContent = '다크 모드';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        themeText.textContent = '라이트 모드';
+        localStorage.setItem('theme', 'light');
+    }    
+};
+
+// 이벤트 리스너
 generateBtn.addEventListener('click', () => {
     const numbers = generateNumbers();
     displayNumbers(numbers);
 });
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// 초기화
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        themeText.textContent = '다크 모드';
+    }
+}
 
 displayNumbers(generateNumbers());
